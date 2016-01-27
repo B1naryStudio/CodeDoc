e5124d14e343010a4fddf4f80bbfc23b64836f7c
Anatoliy Sieryi <jacobator@gmail.com>
2016-01-27 17:14:45 +0200

    main and result windows scrolling synced, style fixes

--- app/components/MainWindow.js
+++ app/components/MainWindow.js
@@ -4,6 +4,8 @@ import * as mainWindowActions from '../actions/mainWindow'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+let dispatchScrollEvent = true;
+
 export default class MainWindow extends Component {
 	static defaultProps = {initialValue: ''};
 
@@ -34,6 +36,26 @@ export default class MainWindow extends Component {
 
 	componentDidMount() {
 		this._textarea.focus();
+		this._textarea.addEventListener('scroll', this.handleScroll);
+		this._textarea.addEventListener('res-scroll', this.handleResScroll.bind(this));
+	}
+
+	componentWillUnmount() {
+		this._textarea.removeEventListener('scroll', this.handleScroll);
+		this._textarea.removeEventListener('res-scroll', this.handleResScroll.bind(this));
+	}
+
+	handleScroll(event) {
+		if (dispatchScrollEvent) {
+			document.getElementsByClassName('res-window-container')[0].dispatchEvent(new Event('main-scroll'));
+		}
+	}
+
+	handleResScroll(event) {
+		var res = document.getElementsByClassName('res-window-container')[0];
+		dispatchScrollEvent = false;
+		this._textarea.scrollTop = (res.scrollTop / (res.scrollHeight - res.offsetHeight)) * (this._textarea.scrollHeight - this._textarea.offsetHeight);
+		setTimeout(function() {dispatchScrollEvent = true}, 100);
 	}
 
 	render() {
--- app/components/ResultWindow.js
+++ app/components/ResultWindow.js
@@ -3,10 +3,41 @@ import { Link } from 'react-router';
 import Markdown from'react-remarkable';
 import { connect } from 'react-redux'
 
+let dispatchScrollEvent = true;
+
 export default class ResultWindow extends Component {
+
+	componentDidMount() {
+		var res = document.getElementsByClassName('res-window-container')[0];
+
+		res.addEventListener('scroll', this.handleScroll);
+		res.addEventListener('main-scroll', this.handleMainScroll);
+	}
+
+	componentWillUnmount() {
+		var res = document.getElementsByClassName('res-window-container')[0];
+		
+		res.removeEventListener('scroll', this.handleScroll);
+		res.removeEventListener('main-scroll', this.handleMainScroll);
+	}
+
+	handleScroll(event) {
+		if (dispatchScrollEvent) {
+			document.getElementsByTagName('textarea')[0].dispatchEvent(new Event('res-scroll'));
+		}
+	}
+
+	handleMainScroll(event) {
+		var res = document.getElementsByClassName('res-window-container')[0];
+		var main = document.getElementsByTagName('textarea')[0];
+		dispatchScrollEvent = false;
+		res.scrollTop = (main.scrollTop / (main.scrollHeight - main.offsetHeight)) * (res.scrollHeight - res.offsetHeight);
+		setTimeout(function() {dispatchScrollEvent = true}, 100);
+	}
+
 	render() {
 		return (
-			<div>
+			<div ref={(resWindow) => this._resWindow = resWindow}>
 				<Markdown> 
 					{this.props.mainWindow.mainWindowText}
 				</Markdown>
