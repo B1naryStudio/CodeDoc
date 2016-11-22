20bbc426b9a1750a5b3df2a2dc202a3dae11ba3b
Vasyl Terletskyi <vasyl.terletskyi@binary-studio.com>
2016-11-22 09:44:35 +0200

    Generate  html bundle

--- app/components/Toolbar.js
+++ app/components/Toolbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import * as mainWindowActions from '../actions/mainWindow'
+import { exportAllToHTML } from '../markdownConverter/markdownConverter'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
@@ -14,67 +15,89 @@ class Toolbar extends Component {
 	undoAction() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.undoAction();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	redoAction() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.redoAction();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
-	addBoldText() {		
+	addBoldText() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addBoldText();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addItalicText() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addItalicText();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addBlockQuote() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addBlockQuote();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addComment() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addComment();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addCodeStyle() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addCodeStyle();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addSimpleList() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addSimpleList();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addNumList() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addNumList();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addHorizRule() {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addHorizRule();
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addHeaderToText(count) {
 		var textarea = document.getElementsByTagName('textarea')[0];
 		this.props.addHeader(count);
-		setTimeout(function(){textarea.focus()}, 0);
+		setTimeout(function() {
+			textarea.focus()
+		}, 0);
 	}
 
 	addLinkToText() {
@@ -85,33 +108,39 @@ class Toolbar extends Component {
 		this.props.showModalWindow('imageLink');
 	}
 
+	exportToHTML() {
+		exportAllToHTML(this.props.basePath);
+	}
+
 	render() {
 		return (
-				<div className="btn-group" role="group" aria-label="...">
-					<button disabled={!this.props.mainWindow.pastStates.length} className="btn btn-default" onClick={this.undoAction.bind(this)} data-toggle="tooltip" title="Undo"><i className="fa fa-reply"></i></button>
-					<button disabled={!this.props.mainWindow.futureStates.length} className="btn btn-default" onClick={this.redoAction.bind(this)} data-toggle="tooltip" title="Redo"><i className="fa fa-share"></i></button>
-					<button className="btn btn-default" onClick={this.addBoldText.bind(this)} data-toggle="tooltip" title="Bold"><b>B</b></button>
-					<button className="btn btn-default" onClick={this.addItalicText.bind(this)} data-toggle="tooltip" title="Italic"><i>I</i></button>
-					<button className="btn btn-default" onClick={this.addHeaderToText.bind(this, 1)} data-toggle="tooltip" title="Large heading">H1</button>
-					<button className="btn btn-default" onClick={this.addHeaderToText.bind(this, 2)} data-toggle="tooltip" title="Medium heading">H2</button>
-					<button className="btn btn-default" onClick={this.addHeaderToText.bind(this, 3)} data-toggle="tooltip" title="Small heading">H3</button>
-					<button className="btn btn-default" onClick={this.addLinkToText.bind(this)} data-toggle="tooltip" title="Link"><i className="fa fa-link"></i></button>
-					<button className="btn btn-default" onClick={this.addTheImageLinkToText.bind(this)} data-toggle="tooltip" title="Image"><i className="fa fa-image"></i></button>
-					<button className="btn btn-default" onClick={this.addBlockQuote.bind(this)} data-toggle="tooltip" title="Quote"><i className="fa fa-quote-left"></i></button>
-					<button className="btn btn-default" onClick={this.addComment.bind(this)} data-toggle="tooltip" title="Comment"><i className="fa fa-comment"></i></button>
-					<button className="btn btn-default" onClick={this.addCodeStyle.bind(this)} data-toggle="tooltip" title="Code"><i className="fa fa-code"></i></button>
-					<button className="btn btn-default" onClick={this.addSimpleList.bind(this)} data-toggle="tooltip" title="Unodered list"><i className="fa fa-list"></i></button>
-					<button className="btn btn-default" onClick={this.addNumList.bind(this)} data-toggle="tooltip" title="Ordered list"><i className="fa fa-list-ol"></i></button>
-					<button className="btn btn-default" onClick={this.addHorizRule.bind(this)} data-toggle="tooltip" title="Horizontal rule"><i className="fa fa-ellipsis-h"></i></button>
-				</div>
-		);
+			<div className="btn-group" role="group" aria-label="...">
+     <button disabled={ !this.props.mainWindow.pastStates.length } className="btn btn-default" onClick={ this.undoAction.bind(this) } data-toggle="tooltip" title="Undo"><i className="fa fa-reply"></i></button>
+     <button disabled={ !this.props.mainWindow.futureStates.length } className="btn btn-default" onClick={ this.redoAction.bind(this) } data-toggle="tooltip" title="Redo"><i className="fa fa-share"></i></button>
+     <button className="btn btn-default" onClick={ this.addBoldText.bind(this) } data-toggle="tooltip" title="Bold"><b>B</b></button>
+     <button className="btn btn-default" onClick={ this.addItalicText.bind(this) } data-toggle="tooltip" title="Italic"><i>I</i></button>
+     <button className="btn btn-default" onClick={ this.addHeaderToText.bind(this, 1) } data-toggle="tooltip" title="Large heading">H1</button>
+     <button className="btn btn-default" onClick={ this.addHeaderToText.bind(this, 2) } data-toggle="tooltip" title="Medium heading">H2</button>
+     <button className="btn btn-default" onClick={ this.addHeaderToText.bind(this, 3) } data-toggle="tooltip" title="Small heading">H3</button>
+     <button className="btn btn-default" onClick={ this.addLinkToText.bind(this) } data-toggle="tooltip" title="Link"><i className="fa fa-link"></i></button>
+     <button className="btn btn-default" onClick={ this.addTheImageLinkToText.bind(this) } data-toggle="tooltip" title="Image"><i className="fa fa-image"></i></button>
+     <button className="btn btn-default" onClick={ this.addBlockQuote.bind(this) } data-toggle="tooltip" title="Quote"><i className="fa fa-quote-left"></i></button>
+     <button className="btn btn-default" onClick={ this.addComment.bind(this) } data-toggle="tooltip" title="Comment"><i className="fa fa-comment"></i></button>
+     <button className="btn btn-default" onClick={ this.addCodeStyle.bind(this) } data-toggle="tooltip" title="Code"><i className="fa fa-code"></i></button>
+     <button className="btn btn-default" onClick={ this.addSimpleList.bind(this) } data-toggle="tooltip" title="Unodered list"><i className="fa fa-list"></i></button>
+     <button className="btn btn-default" onClick={ this.addNumList.bind(this) } data-toggle="tooltip" title="Ordered list"><i className="fa fa-list-ol"></i></button>
+     <button className="btn btn-default" onClick={ this.addHorizRule.bind(this) } data-toggle="tooltip" title="Horizontal rule"><i className="fa fa-ellipsis-h"></i></button>
+     <button className="btn btn-default" onClick={ this.exportToHTML.bind(this) } data-toggle="tooltip" title="Export to HTML"><i className="fa fa-html5"></i></button>
+   </div>
+			);
 	}
 }
 
 function mapStateToProps(state) {
 	return {
 		toolbarState: state.toolbar,
-		mainWindow: state.mainWindow
+		mainWindow: state.mainWindow,
+		basePath: state.projectWindow.tree.path
 	}
 }
 
--- app/markdownConverter/markdownConverter.js
+++ app/markdownConverter/markdownConverter.js
@@ -2,11 +2,12 @@ export const LOAD_CODE_FILE = 'LOAD_CODE_FILE';
 
 
 const remote = require('remote');
-const electron =  remote.require('electron');
+const electron = remote.require('electron');
+const {shell} = require('electron');
 const fs = require('fs');
 const path = require('path');
 const dialog = electron.dialog;
-const showdown  = require('showdown');
+const showdown = require('showdown');
 
 // import githubMarkdown from './assets/css/githubMarkdown.css';
 // import hljsGithub from './assets/css/hljs-github.min.css';
@@ -19,7 +20,6 @@ const showdown  = require('showdown');
 //const page = require('./markdownConverterTemplates/page.css');
 
 
-
 // const extension = require('showdown-twitter');//('../src/showdown-twitter.js')
 // const mds = require('markdown-styles');
 
@@ -30,38 +30,112 @@ const showdown  = require('showdown');
 // 		cursorPosition: cursorPosition
 // 	};
 // }
+export function exportAllToHTML(basePath) {
+	console.log(showdown.getDefaultOptions());
+	showdown.setOption("tables", true);
+	//showdown.setOption('optionKey', 'value');
+	let converter = new showdown.Converter();
+	let rootFolder = "/_codedoc_html";
+	fs.readFile(basePath + "/.codedoc/docsConfig.json", "utf-8", function(err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			let config = JSON.parse(data);
+			if (!config.contentTree.length) {
+				alert("Nothing to export");
+			} else {
+				//get docs path and content
+				let docs = config.contentTree;
+				let mdContents = [];
+				for (let i = 0; i < docs.length; i++) {
+					try {
+						mdContents[i] = fs.readFileSync(docs[i].docsPath, "utf-8");
+					} catch (e) {
+						alert(e);
+						break;
+					}
+				}
+				let pages = [];
+				let title;
+				//generate pages
+				for (let i = 0; i < docs.length; i++) {
+					if (mdContents[i]) {
+						title = docs[i].docsPath.substring(docs[i].docsPath.indexOf(basePath) + basePath.length + 10);
+						pages.push({
+							title: title.replace("/", "_"),
+							content: converter.makeHtml(mdContents[i])
+						});
+					}
+				}
+				//if some pages
+				if (pages.length) {
+					dialog.showOpenDialog({
+						title: "Select directory for HTML files",
+						properties: ["openDirectory"],
+						defaultPath: "/"
+					}, function(dir) {
+						if (dir) {
+
+							try {
+								fs.mkdirSync(dir + rootFolder);
+							} catch (e) {
+								deleteFolderRecursive(dir + rootFolder);
+								console.log(e);
+								fs.mkdirSync(dir + rootFolder);
+							}
+							let singlePage = fs.readFileSync('./app/markdownConverter/markdownConverterTemplates/singlePage.html', 'utf8');
+							let home = fs.readFileSync('./app/markdownConverter/markdownConverterTemplates/map.html', 'utf8');
+							let links = "";
+							for (let i = 0; i < pages.length; i++) {
+								links += `<li><a href='${pages[i].title+".html"}'>${pages[i].title}</a></li>`;
+								pages[i].content = singlePage.replace("{{content}}", "<a id='back-to-main' href='index.html'>Back to Main</a>" + pages[i].content);
+								pages[i].content = pages[i].content.replace("{{title}}", pages[i].title);
+								fs.writeFileSync(dir + rootFolder + "/" + pages[i].title + ".html", pages[i].content);
+							}
+							let main = home.replace("{{_links}}", links);
+							fs.writeFileSync(dir + rootFolder + "/index.html", main);
+							shell.openExternal("file://" + dir + rootFolder + "/index.html");
+							alert("Files successfuly exported");
+						}
+					});
+				}
+			}
+		}
+	});
+}
 
 export function convertMDtoHTML() {
 	return (dispatch, getStore) => {
 		console.log('converting');
 		let store = getStore();
 		let md_content = store.mainWindow.mainWindowText;
-		let converter = new showdown.Converter();		
+		let converter = new showdown.Converter();
 		let html_content = converter.makeHtml(md_content);
 
-		dialog.showSaveDialog({ 
+		dialog.showSaveDialog({
 			title: 'Save File',
-			filters: [{ 
-				name: 'Web-page', 
-				extensions: ['html'] 
+			filters: [{
+				name: 'Web-page',
+				extensions: ['html']
 			}]
-		}, function (filePath) {
-			if(filePath) {
-				if (filePath.substr(-5,5) !== '.html'){
+		}, function(filePath) {
+			if (filePath) {
+				if (filePath.substr(-5, 5) !== '.html') {
 					filePath += '.html';
 				}
 				fs.readFile('./app/markdownConverter/markdownConverterTemplates/singlePage.html', 'utf8', (err, data) => {
 					if (err) {
 						console.log(err);
-					};
+					}
+					;
 					let html_file = data.replace('{{content}}', html_content);
 					html_file = html_file.replace('{{title}}', path.basename(store.mainWindow.currentLink, '.md'));
-					fs.writeFile(filePath, html_file, function (err) {
-						if(err) {
+					fs.writeFile(filePath, html_file, function(err) {
+						if (err) {
 							console.log(err);
 						} else {
 							console.log('saved');
-						}				
+						}
 					});
 				});
 			} else {
@@ -70,3 +144,17 @@ export function convertMDtoHTML() {
 		});
 	};
 }
+
+var deleteFolderRecursive = function(path) {
+	if (fs.existsSync(path)) {
+		fs.readdirSync(path).forEach(function(file, index) {
+			var curPath = path + "/" + file;
+			if (fs.lstatSync(curPath).isDirectory()) { // recurse
+				deleteFolderRecursive(curPath);
+			} else { // delete file
+				fs.unlinkSync(curPath);
+			}
+		});
+		fs.rmdirSync(path);
+	}
+};
